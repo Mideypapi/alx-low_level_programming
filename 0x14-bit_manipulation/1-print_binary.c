378c1df79bed1d2f3886e75c7a9a7d5bf82ad482
Mideypapi <abioye.faruok@gmail.com>
2023-11-23 14:52:09 -0800

    first

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,1 +1,27 @@
+#include "main.h"
 
+/**
+ * print_binary - write a function that
+ * prints d binary representation of a number.
+ *
+ * @n: number
+ * Return: void is returned
+ */
+void print_binary(unsigned long int n)
+{
+	char i;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	print_binary(n >> 1);
+	i = (n & 1) ? '1' : '0';
+	_putchar(i);
+}
