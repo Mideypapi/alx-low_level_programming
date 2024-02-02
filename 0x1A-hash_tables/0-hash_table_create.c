36a9d2ad20c0d0975eafad627df4365dd1791577
mideypapi <abioye.faruok@gmail.com>
2024-02-02 23:07:53 +0000

    task1 push

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Function to create d hash table
+ *
+ * @size: size of d hash table
+ *
+ * Return: a pointer to the newly created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hashtable = NULL;
+	unsigned long int i;
+
+	if (size <= 0)
+		return (NULL);
+
+	hashtable = malloc(sizeof(hash_table_t));
+	if (hashtable == NULL)
+		return (NULL);
+
+	hashtable->size = size;
+
+	hashtable->array = malloc(sizeof(hash_node_t *) * size);
+		if (hashtable->array == NULL)
+			return (NULL);
+
+	for (i = 0; i < size; i++)
+		hashtable->array[i] = NULL;
+
+	return (hashtable);
+}
